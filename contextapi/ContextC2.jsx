92f71b679880e710c9cd65a2698af163076cc37c
AnjaliAnreddy <anreddyanjali200427@gmail.com>
2025-02-15 15:34:00 +0530

    react codes

--- contextapi/ContextC2.jsx
+++ contextapi/ContextC2.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import ContextC3 from './ContextC3'
-import { GlobalDataProvider } from './ContextAPI.js';
+import { GlobalDataProvider } from './ContextAPI';
 
 const ContextC2 = () => {
     const data = useContext(GlobalDataProvider);
